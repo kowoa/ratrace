f70f58dc0f430f847f60bf173345f45b8f06d4bd
Khoa Dang Hoang <koadahoakodah@gmail.com>
2024-05-04 15:51:17 -0400

    Add Ray struct

--- src/ray_tracer/mod.rs
+++ src/ray_tracer/mod.rs
@@ -1,19 +1,20 @@
+mod ray;
+
 use minifb::{Key, Window, WindowOptions};
 use rand::prelude::*;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
-
-const WIDTH: usize = 256;
-const HEIGHT: usize = 256;
 
 pub fn run() {
     color_eyre::install().unwrap();
     env_logger::init();
 
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const WIDTH: u32 = 400;
+    const HEIGHT: u32 = (WIDTH as f64 / ASPECT_RATIO) as u32;
+
     let buffer: Vec<u32> = (0..WIDTH * HEIGHT)
-        //.into_par_iter()
-        //.map_init(thread_rng, |rng, i| {
-        .into_iter()
-        .map(|i| {
+        .into_par_iter()
+        .map_init(thread_rng, |rng, i| {
             let x = i % WIDTH;
             let y = i / WIDTH;
 
@@ -21,35 +22,31 @@ pub fn run() {
             let g = y as f64 / (HEIGHT - 1) as f64;
             let b = 0.0;
 
-            let ir = (255.999 * r) as u8;
-            let ig = (255.999 * g) as u8;
-            let ib = (255.999 * b) as u8;
-
-            // BGRA
-            u32::from_le_bytes([ib, ig, ir, 255])
+            color_f(r, g, b)
         })
         .collect();
-    /*
-        let mut buffer: Vec<u8> = Vec::with_capacity(WIDTH * HEIGHT * 4);
-        for j in 0..HEIGHT {
-            for i in 0..WIDTH {
-                let r = i as f64 / (WIDTH - 1) as f64;
-                let g = j as f64 / (HEIGHT - 1) as f64;
-                let b = 0.0;
-                let ir = (255.999 * r) as u8;
-                let ig = (255.999 * g) as u8;
-                let ib = (255.999 * b) as u8;
-                // BGRA
-                buffer.push(ib);
-                buffer.push(ig);
-                buffer.push(ir);
-                buffer.push(255);
-            }
-        }
-    */
 
-    let mut window = Window::new("Ray Tracer", WIDTH, HEIGHT, WindowOptions::default()).unwrap();
+    let mut window = Window::new(
+        "Ray Tracer",
+        WIDTH as usize,
+        HEIGHT as usize,
+        WindowOptions::default(),
+    )
+    .unwrap();
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+        window
+            .update_with_buffer(&buffer, WIDTH as usize, HEIGHT as usize)
+            .unwrap();
     }
+}
+
+fn color(r: u8, g: u8, b: u8) -> u32 {
+    u32::from_le_bytes([b, g, r, 255])
+}
+
+fn color_f(r: f64, g: f64, b: f64) -> u32 {
+    let ir = (255.999 * r) as u8;
+    let ig = (255.999 * g) as u8;
+    let ib = (255.999 * b) as u8;
+    color(ir, ig, ib)
 }
---
+++ src/ray_tracer/ray.rs
@@ -0,0 +1,16 @@
+use nalgebra_glm::Vec3;
+
+struct Ray {
+    origin: Vec3,
+    direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Vec3, direction: Vec3) -> Self {
+        Self { origin, direction }
+    }
+
+    pub fn at(&self, t: f32) -> Vec3 {
+        self.origin + self.direction * t
+    }
+}
