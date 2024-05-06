3662c56dab39d5081c0fd7037fd00c6ca40161e4
kowoa <koadahoakodah@gmail.com>
2024-05-06 00:10:14 -0400

    Render red sphere

--- src/ray_tracer/mod.rs
+++ src/ray_tracer/mod.rs
@@ -16,13 +16,10 @@ struct Camera {
 }
 
 impl Camera {
-    const ASPECT_RATIO: f32 = 16.0 / 9.0;
-}
-
-impl Default for Camera {
-    fn default() -> Self {
+    pub fn new(image_width: u32, image_height: u32) -> Self {
         let viewport_height: f32 = 2.0;
-        let viewport_width: f32 = viewport_height * Self::ASPECT_RATIO;
+        let viewport_width: f32 = viewport_height * (image_width as f32 / image_height as f32);
+
         Self {
             eye: Vec3::new(0.0, 0.0, 0.0),
             focal_length: 1.0,
@@ -37,10 +34,10 @@ pub fn run() {
     env_logger::init();
 
     let image_aspect_ratio: f32 = 16.0 / 9.0;
-    let image_width: u32 = 400;
+    let image_width: u32 = 800;
     let image_height: u32 = std::cmp::max((image_width as f32 / image_aspect_ratio) as u32, 1);
 
-    let camera = Camera::default();
+    let camera = Camera::new(image_width, image_height);
     let viewport_u = vec3(camera.viewport_width, 0.0, 0.0);
     let viewport_v = vec3(0.0, -camera.viewport_height, 0.0);
     let pixel_delta_u = viewport_u / image_width as f32;
@@ -54,7 +51,7 @@ pub fn run() {
         .into_par_iter()
         .map_init(thread_rng, |_rng, i| {
             let u = i as f32 % image_width as f32;
-            let v = i as f32 / image_height as f32;
+            let v = i as f32 / image_width as f32;
 
             let pixel_center = pixel00_center + u * pixel_delta_u + v * pixel_delta_v;
             let ray_direction = pixel_center - camera.eye;
--- src/ray_tracer/ray.rs
+++ src/ray_tracer/ray.rs
@@ -18,8 +18,21 @@ impl Ray {
 
     /// Returns the color of the pixel that the ray hits
     pub fn trace(&self) -> Color {
+        if self.hit_sphere(vec3(0.0, 0.0, -1.0), 0.5) {
+            return Color::new(1.0, 0.0, 0.0);
+        }
+
         let unit_direction = self.direction.normalize();
         let t = 0.5 * (unit_direction.y + 1.0);
         Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
+    }
+
+    fn hit_sphere(&self, center: Vec3, radius: f32) -> bool {
+        let oc = center - self.origin;
+        let a = self.direction.magnitude_squared();
+        let b = -2.0 * self.direction.dot(&oc);
+        let c = oc.magnitude_squared() - radius * radius;
+        let discriminant = b * b - 4.0 * a * c;
+        discriminant >= 0.0
     }
 }
